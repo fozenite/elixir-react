841fe62005465e0de47eb14407278bedd0049c4a
Rohit <moza.rohit@gmail.com>
2018-02-25 15:26:58 -0500

    Setup Routes and used react-router-dom instead

---
+++ web/src/containers/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from '../Home';
+import NotFound from '../../components/NotFound';
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
